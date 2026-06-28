8198b3faf19bc6d4bc56b5b949719e76f764c5b2
Vivek Sahu <sahuvivek1408@gmail.com>
2026-06-28 15:22:00 +0530

    Fixed STATIC_ROOT path for render deployment2

--- garagego/settings.py
+++ garagego/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-3_xy3-dy%m=2ma&4vjux!g*noifl=n^z9w=)sad&1cleq&+md)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
